d77916b9230f90b3d1ff6ff717bbe95a5a2ae8d4
rexwzh <1073853456@qq.com>
2025-04-01 02:35:24 +0800

    update projects

--- client/src/config/config.tsx
+++ client/src/config/config.tsx
@@ -3,20 +3,13 @@ import { LeanWebConfig } from './docs' // look here for documentation of the ind
 const lean4webConfig : LeanWebConfig = {
   "projects": [
     { "folder": "mathlib-demo",
-      "name": "Latest Mathlib",
-      "examples": [
-        { "file": "MathlibLatest/Bijection.lean",
-          "name": "Bijection" },
-        { "file": "MathlibLatest/Logic.lean",
-          "name": "Logic" },
-        { "file": "MathlibLatest/Ring.lean",
-          "name": "Ring" },
-        { "file": "MathlibLatest/Rational.lean",
-          "name": "Rational" }]},
-    { "folder": "stable",
-      "name": "Stable Lean" },
-    { "folder": "nightly",
-        "name": "Nightly" }
+      "name": "v4.17.0",},
+    { "folder": "v4.14.0",
+      "name": "v4.14.0",},
+    { "folder": "v4.10.0",
+      "name": "v4.10.0"},
+    { "folder": "lips-0324",
+      "name": "lips-0324"}
   ],
   "serverCountry": null,
   "contactDetails": null,
