c476fe787ab7aae75c94418b4a119a8842fa989b
rexwzh <1073853456@qq.com>
2026-01-25 18:14:40 +0800

    update projects

--- client/src/config/config.tsx
+++ client/src/config/config.tsx
@@ -1,22 +1,24 @@
 import { LeanWebConfig } from './docs' // look here for documentation of the individual config options
 
-const lean4webConfig: LeanWebConfig = {
-  projects: [
-    {
-      folder: 'MathlibDemo',
-      name: 'Latest Mathlib',
-      examples: [
-        { file: 'MathlibDemo/Bijection.lean', name: 'Bijection' },
-        { file: 'MathlibDemo/Logic.lean', name: 'Logic' },
-        { file: 'MathlibDemo/Ring.lean', name: 'Ring' },
-        { file: 'MathlibDemo/Rational.lean', name: 'Rational' },
-      ],
-    },
-    { folder: 'Stable', name: 'Stable Lean' },
+const lean4webConfig : LeanWebConfig = {
+  "projects": [
+    { "folder": "MathlibDemo",
+      "name": "v4.22.0"},
+    // { "folder": "v4.26.0",
+    //   "name": "v4.26.0"},
+    // { "folder": "v4.24.0",
+    //   "name": "v4.24.0"},
+    // { "folder": "v4.21.0",
+    //   "name" : "v4.21.0"},
+    // { "folder": "v4.19.0",
+    //     "name": "v4.19.0" },
+    // { "folder": "v4.17.0",
+    //     "name": "v4.17.0" },
+    // { "folder": "v4.14.0",
+    //     "name": "v4.14.0" },
   ],
-  serverCountry: null,
-  contactDetails: null,
-  impressum: null,
+  "serverCountry": null,
+  "contactDetails": null,
+  "impressum": null
 }
-
 export default lean4webConfig
